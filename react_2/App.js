1003ec35c5ffe4e3c30f5245fc793bc1c0a74d2f
michalprzyl <michal.przyl@gmail.com>
2021-06-14 13:23:11 +0200

    Finished 2

--- react_2/App.js
+++ react_2/App.js
@@ -1,7 +1,28 @@
 class Message extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messageIsActive: false
+    }
+    this.handleMessageButton = this.handleMessageButton.bind(this);
+  }
+
+  handleMessageButton() {
+    this.setState({
+      messageIsActive: !this.state.messageIsActive
+    })
+
+  }
+
   render() {
+    console.log(this.state.messageIsActive);
+    const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Phasellus fermentum urna eget laoreet vestibulum.Nunc quis ligula bibendum, consectetur urna vitae, consequat ligula.Cras venenatis viverra nibh ut mollis.Vivamus scelerisque felis est, eget pharetra tellus sodales ac.Sed porttitor urna sed felis consequat pellentesque.Curabitur quis felis nibh.Donec pellentesque lectus vel lobortis fermentum.';
+
     return (
-      <h1>Hej</h1>
+      <React.Fragment>
+        <button onClick={this.handleMessageButton}>{this.state.messageIsActive ? 'Ukryj' : 'Pokaż'}</button>
+        {this.state.messageIsActive ? <p>{text}</p> : null}
+      </React.Fragment>
     )
   }
 }
